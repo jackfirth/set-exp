b58671af6b7b27f4e0028f1f19f25b9498ea330a
Jack Firth <jackhfirth@gmail.com>
2015-02-23 09:56:48 -0800

    Fix dependencies

--- info.rkt
+++ info.rkt
@@ -1,4 +1,9 @@
 #lang info
 
 (define collection 'multi)
-(define deps '("base"))
+(define deps '("base" "rackunit-lib" "doc-coverage"))
+(define build-deps '("cover"
+                     "doc-coverage"
+                     "scribble-lib"
+                     "rackunit-lib"
+                     "racket-doc"))
